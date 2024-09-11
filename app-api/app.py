2f5c8e0d1085a0f53a6e3c0a07f94bf782843f74
nehasaini1608 <nehasaini@Nehas-MacBook-Pro.local>
2024-09-11 13:00:23 +0530

    Add semnatic search files to the repository

---
+++ app-api/app.py
@@ -0,0 +1,262 @@
+from flask import Flask, request, jsonify
+import os
+from elasticsearch import Elasticsearch
+import logging
+
+ES_URL = os.environ["ES_URL"]
+ES_USER = os.environ["ELASTICSEARCH_USERNAME"]
+ES_PASSWORD = os.environ["ELASTICSEARCH_PASSWORD"]
+logging.basicConfig(
+    level=logging.DEBUG,                 # Set logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    format='%(asctime)s - %(levelname)s - %(message)s',  # Define the log format
+    handlers=[
+        logging.FileHandler("app.log"),  # Write logs to a file called 'app.log'
+        logging.StreamHandler()          # Also print logs to the console
+    ]
+)
+
+client = Elasticsearch(hosts=ES_URL, basic_auth=(ES_USER, ES_PASSWORD))
+
+
+datasets = {
+    "movies": {
+        "id": "movies",
+        "label": "Movies",
+        "index": "search-movies",
+        "search_fields": ["title"],
+        "semantic_search_field": "title_vector",
+        "result_fields": ["title", "overview"],
+        "mapping_fields": {"text": "overview", "title": "title"},
+    }
+}
+
+app = Flask(__name__)
+
+
+
+@app.route("/api/search/<index>")
+def route_api_search(index):
+    """
+    Execute the search
+    """
+    [query, hybrid, type, k, datasetId] = [
+        request.args.get("q"),
+        request.args.get("hybrid", default=False, type=lambda v: v.lower() == "true"),
+        request.args.get("type", default="bm25"),
+        request.args.get("k", default=0),
+        request.args.get("dataset", default="movies"),
+    ]
+
+
+    if type == "semantic":
+        search_result = run_semantic_search(
+            query, index, **{"hybrid": hybrid, "k": k, "dataset": datasetId}
+        )
+    elif type == "bm25":
+        search_result = run_full_text_search(query, index, **{"dataset": datasetId})
+    
+
+    transformed_search_result = transform_search_response(
+        search_result, datasets[datasetId]["mapping_fields"]
+    )
+    return jsonify(response=transformed_search_result)
+
+
+@app.route("/api/datasets", methods=["GET"])
+def route_api_datasets():
+    """
+    Return the available datasets
+    """
+    return datasets
+
+
+@app.errorhandler(404)
+def resource_not_found(e):
+    """
+    Return a JSON response of the error and the URL that was requested
+    """
+    return jsonify(error=str(e)), 404
+
+
+
+def get_semantic_request_body(query, size=10, **options):
+    """
+    Generates an ES semantic search request.
+    """
+
+    fields = datasets[options["dataset"]]["result_fields"]
+    search_field = datasets[options["dataset"]]["semantic_search_field"]
+    return {
+        "_source": False,
+        "fields": fields,
+        "size": size,
+        "query": {"semantic": {"query": query, "field": search_field}},
+    }
+
+
+def get_hybrid_request_body(query, size=10, **options):
+    """
+    Generates an ES semantic search request.
+    """
+
+    fields = datasets[options["dataset"]]["result_fields"]
+    semantic_search_field = datasets[options["dataset"]]["semantic_search_field"]
+    search_fields = datasets[options["dataset"]]["search_fields"]
+    return {
+        "_source": False,
+        "fields": fields,
+        "size": size,
+        "query": {
+            "bool": {
+                "should": [{
+                    "multi_match": {
+                        "fields": search_fields,
+                        "query": query,
+                        "boost": 1.5,
+                    }
+                },
+                 {
+                    "semantic": {
+                        "field": semantic_search_field,
+                        "query": query,
+                        "boost": 3.0,
+                    }
+                }]
+            }
+        },
+    }
+
+
+def get_text_search_request_body(query, size=10, **options):
+    """
+    Generates an ES full text search request.
+    """
+    fields = datasets[options["dataset"]]["result_fields"]
+    search_fields = datasets[options["dataset"]]["search_fields"]
+    return {
+        "_source": False,
+        "fields": fields,
+        "size": size,
+        "query": {"multi_match": {"query": query, "fields": search_fields}},
+    }
+
+
+def get_hybrid_search_rrf_request_body(query, size=10, **options):
+    """
+    Generates an ES hybrid search with RRF
+    """
+    fields = datasets[options["dataset"]]["elser_search_fields"]
+    result_fields = datasets[options["dataset"]]["result_fields"]
+    search_fields = datasets[options["dataset"]]["search_fields"]
+    text_expansions = []
+    boost = 1
+
+    for field in fields:
+
+        split_field_descriptor = field.split("^")
+        if len(split_field_descriptor) == 2:
+            boost = split_field_descriptor[1]
+            field = split_field_descriptor[0]
+        te = {"text_expansion": {}}
+        te["text_expansion"][field] = {
+            "model_text": query,
+            "model_id": ".elser_model_1",
+            "boost": boost,
+        }
+        text_expansions.append(te)
+    return {
+        "_source": False,
+        "fields": result_fields,
+        "size": size,
+        "rank": {"rrf": {"window_size": 10, "rank_constant": 2}},
+        "sub_searches": [
+            {"query": {"bool": {"should": text_expansions}}},
+            {"query": {"multi_match": {"query": query, "fields": search_fields}}},
+        ],
+    }
+
+
+def execute_search_request(index, body):
+    """
+    Executes an ES search request and returns the JSON response.
+    """
+    logging.info("execute_search_request");
+    logging.info(body);
+
+    response = client.search(
+        index=index,
+        query=body["query"],
+        fields=body["fields"],
+        size=body["size"],
+        source=body["_source"],
+    )
+
+    return response
+
+
+def execute_search_request_using_raw_dsl(index, body):
+    """
+    Executes an ES search request using the request library and returns the JSON response.
+    """
+      
+    response = client.perform_request(
+        "POST",
+        f"/{index}/_search",
+        headers={"content-type": "application/json", "accept": "application/json"},
+        body=body,
+    )
+
+    return response
+
+
+def run_full_text_search(query, index, **options):
+    """
+    Runs a full text search on the given index using the passed query.
+    """
+    if query is None or query.strip() == "":
+        raise Exception("Query cannot be empty")
+    body = get_text_search_request_body(query, **options)
+    response = execute_search_request(index, body)
+
+    return response["hits"]["hits"]
+
+
+def run_semantic_search(query, index, **options):
+    """
+    Runs a semantic search of the provided query on the target index, and reranks the KNN and BM25 results.
+    """
+
+    if options.get("hybrid") == True:
+        logging.info("hybrid search");
+        body = get_hybrid_request_body(query, **options)
+        # Execute the request using the raw DSL to avoid the ES Python client since sub_searches query are not supported yet
+        response_json = execute_search_request(index, body)
+    else:
+        body = get_semantic_request_body(query, **options)
+        logging.info("body");
+        logging.info(body);
+        response_json = execute_search_request(index, body)
+
+    return response_json["hits"]["hits"]
+
+
+
+def find_id_index(id: int, hits: list):
+    """
+    Finds the index of an object in `hits` which has _id == `id`.
+    """
+
+    for i, v in enumerate(hits):
+        if v["_id"] == id:
+            return i + 1
+    return 0
+
+
+def transform_search_response(searchResults, mappingFields):
+    for hit in searchResults:
+        fields = hit["fields"]
+        hit["fields"] = {
+            "text": fields[mappingFields["text"]],
+            "title": fields[mappingFields["title"]],
+        }
+    return searchResults
---
+++ app-ui/src/components/ResultSet.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import classNames from 'classnames';
+
+import BackgroundPattern from './BackgroundPattern';
+import RRFToggle from './RRFToggle';
+import SearchResult from './SearchResult';
+
+export default function ResultSet({ index, type, results, enableHybrid, setEnableHybrid, activeResult, setActiveResult, globalSettings }) {
+  const { theme, id, label, hybrid_capable, showChange } = type;
+  const resultSetClasses = classNames(
+    "p-8 relative", {
+      "border-4 border-elastic-blue": theme === "dark",
+      "shadow-lg bg-elastic-blue-light": theme === "light",
+    }
+  )
+
+  console.log("enableHybrid",enableHybrid);
+  console.log("Results:", results);
+  return (
+    <div className={resultSetClasses}>
+      <BackgroundPattern theme={theme} />
+      <div className="flex justify-between items-center mb-4">
+        <h2 className="text-2xl font-bold text-white">{label}</h2>
+        {(hybrid_capable && results.length > 0) && (
+          <div className="flex items-center gap-4">
+            <RRFToggle enableHybrid={enableHybrid} setEnableHybrid={setEnableHybrid} />
+            <p className="text-sm text-white font-medium">Enable Hybrid Search</p>
+          </div>
+        )}
+      </div>
+     
+      {results.map((result) => (
+        <SearchResult
+          key={result._id}
+          theme={theme}
+          result={result}
+          activeResult={activeResult}
+          setActiveResult={setActiveResult}
+          globalSettings={globalSettings}
+          showChange={showChange}
+          typeId={id}
+        />
+      ))}
+    </div>
+  );
+}
